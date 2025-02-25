44407b9007536bd3ec85a23fa5f64fcba37965df
reginaldo <41450286+rvidallima@users.noreply.github.com>
2025-02-25 19:19:00 -0300

    projeto j2

---
+++ exerciciopbasicos.js
@@ -0,0 +1,14 @@
+let x = 1
+let y = '1'
+
+if ( x == y)
+    console.log("verdade")
+
+if (x === y )
+    console.log("false")
+
+if (x >= 10)
+    console.log("verdade")
+else
+    console.log("false")
+
