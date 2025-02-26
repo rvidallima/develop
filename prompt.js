a231d3609f603ddc955c71b27a46a06b7e880411
reginaldo <41450286+rvidallima@users.noreply.github.com>
2025-02-25 21:44:38 -0300

    projeto j5

--- prompt.js
+++ prompt.js
@@ -2,4 +2,4 @@ let valor1 = prompt('digite o primeiro valor:')
 let valor2 = prompt('digite o segundo valor:')
 
 let soma = parseFloat(valor1) + parseFloat(valor2)
-alert(`A soma dos dois valores: ${soma}`) 
+alert(`A soma dos dois valores é: ${soma}`) 
